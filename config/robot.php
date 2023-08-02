153b2a82ce957023b28a84768121831baa2bdc45
mitoop <me@mitoop.com>
2023-08-02 22:41:50 +0800

    配置调整
    Signed-off-by: mitoop <me@mitoop.com>

--- config/robot.php
+++ config/robot.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'default' => ['feishu.jishu'],
+    'default' => ['feishu.demo'],
+    'show_env' => true,
     'env' => env('APP_ENV', 'production'),
     'timeout' => 3,
     'channels' => [
@@ -9,23 +10,20 @@ return [
             'demo' => [
                 'webhook' => '00000000-0000-0000-0000-000000000000',
                 'at' => ['all'],
-                'secret' => '',
-                'show_env' => true,
+                'secret' => ''
             ],
         ],
         'wecom' => [
             'demo' => [
                 'webhook' => 'key',
                 'at' => ['all'],
-                'show_env' => true,
             ],
         ],
         'dingding' => [
             'demo' => [
                 'webhook' => 'access_token',
                 'at' => ['all'],
-                'secret' => '',
-                'show_env' => true,
+                'secret' => ''
             ],
         ],
     ],
